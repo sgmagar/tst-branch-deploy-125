fc82f2597389891f89a73ea6ff55c6f837678ba2
Saurav <sp.gharti@gmail.com>
2022-06-23 16:49:19 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "tst_branch_deploy_125",
+  url: "https://tst_branch_deploy_125.botics.co",
+  api: "https://tst_branch_deploy_125.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
